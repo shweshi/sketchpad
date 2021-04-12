e26f333b19dc7d63639b7fd6b3d71dd4480b5ddc
shweshi <contactmespg@gmail.com>
2021-04-13 03:10:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# drawingpad
+
+Drawing Pad is an easy and clean online drawing tool.
+
+# Features
+
+### Free & Open-Source
+- No Ads. Open-Source.
+
+### Offline
+- All data are saved in your device. No need to communicate with servers.
+
+### Customizable
+- Theme can be customize as per your choice.
+
+## Usages
+Use online here: https://drawingpad.shashi.dev
+
+## Tech
+* HTML, CSS, JavaScript
+
+## Development
+Want to contribute? Great! Feel free to open a PR.
+
+## Happy Coding!!!
---
+++ src/js/main.js
@@ -0,0 +1,111 @@
+var canvas, context, colors, current, drawing = false;
+function init() {
+    canvas = document.getElementsByClassName('whiteboard')[0];
+    colors = document.getElementsByClassName('color');
+    context = canvas.getContext('2d');
+    current = {
+        x: 0,
+        y: 0,
+        color: '',
+    };
+
+    canvas.addEventListener('mousedown', onMouseDown, false);
+    canvas.addEventListener('mouseup', onMouseUp, false);
+    canvas.addEventListener('mouseout', onMouseUp, false);
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+
+    //Touch support for mobile devices
+    canvas.addEventListener('touchstart', onMouseDown, false);
+    canvas.addEventListener('touchend', onMouseUp, false);
+    canvas.addEventListener('touchcancel', onMouseUp, false);
+    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+
+    window.addEventListener('resize', onResize, false);
+    onResize();
+}
+
+function drawLine(x0, y0, x1, y1, color) {
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+}
+
+function onMouseDown(e) {
+    drawing = true;
+    current.x = e.clientX || e.touches[0].clientX;
+    current.y = e.clientY || e.touches[0].clientY;
+}
+
+function onMouseUp(e) {
+    if (!drawing) { return; }
+    drawing = false;
+    drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+}
+
+function onMouseMove(e) {
+    if (!drawing) { return; }
+    drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+    current.x = e.clientX || e.touches[0].clientX;
+    current.y = e.clientY || e.touches[0].clientY;
+}
+
+function onDrawingEvent(data) {
+    var w = canvas.width;
+    var h = canvas.height;
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+}
+
+function onResize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+
+function onColorUpdate(color) {
+    current.color = color;
+}
+
+function onClear() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function save() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+}
+
+function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function () {
+        var time = new Date().getTime();
+
+        if ((time - previousCall) >= delay) {
+            previousCall = time;
+            callback.apply(null, arguments);
+        }
+    };
+}
+
+init();
+
+function printConsoleArt() {
+    const consoleStr = `
+    
+        ██████  ██████   █████  ██     ██ ██ ███    ██  ██████  ██████   █████  ██████      
+        ██   ██ ██   ██ ██   ██ ██     ██ ██ ████   ██ ██       ██   ██ ██   ██ ██   ██     
+        ██   ██ ██████  ███████ ██  █  ██ ██ ██ ██  ██ ██   ███ ██████  ███████ ██   ██     
+        ██   ██ ██   ██ ██   ██ ██ ███ ██ ██ ██  ██ ██ ██    ██ ██      ██   ██ ██   ██     
+        ██████  ██   ██ ██   ██  ███ ███  ██ ██   ████  ██████  ██      ██   ██ ██████      
+                                                                                            
+        Github: https://github.com/shweshi/drawingpad
+        `
+    console.log(consoleStr);
+}
+
+printConsoleArt();
